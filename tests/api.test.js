e469de354f9cdd9e65b6f8864836d45a5913eb48
Aditi Toshniwal <adititoshniwal5@gmail.com>
2020-04-15 22:08:29 +0800

    add tooling. add nyc. add tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -6,7 +6,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
 const app = require('../src/app')(db);
-const buildSchemas = require('../src/schemas');
+const { buildSchemas, insertMockRides } = require('../src/schemas');
 
 describe('API tests', () => {
   before((done) => {
@@ -16,7 +16,7 @@ describe('API tests', () => {
       }
 
       buildSchemas(db);
-
+      insertMockRides(db);
       return done();
     });
   });
@@ -27,6 +27,152 @@ describe('API tests', () => {
         .get('/health')
         .expect('Content-Type', /text/)
         .expect(200, done);
+    });
+  });
+
+  describe('Get /rides', () => {
+    it('should get rides', (done) => {
+      request(app)
+        .get('/rides?prev=102')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect((res) => {
+          const reducer = (acc, elem) => (acc > elem.rideID ? elem.rideID : acc);
+          const firstRideID = res.body.reduce(reducer, Number.MAX_SAFE_INTEGER);
+          res.body = { length: res.body.length, firstRideID };
+        })
+        .expect(200, { length: 100, firstRideID: 103 }, done);
+    });
+    it('should get rides without prevID', (done) => {
+      request(app)
+        .get('/rides')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect((res) => {
+          const reducer = (acc, elem) => (acc > elem.rideID ? elem.rideID : acc);
+          const firstRideID = res.body ? res.body.reduce(reducer, Number.MAX_SAFE_INTEGER) : -1;
+          res.body = { length: res.body.length, firstRideID };
+        })
+        .expect(200, { length: 100, firstRideID: 1 }, done);
+    });
+    it('should return empty array for ID which  does not exist in DB', (done) => {
+      request(app)
+        .get('/rides?prev=102000')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, {
+          error_code: 'RIDES_NOT_FOUND_ERROR',
+          message: 'Could not find any rides',
+        }, done);
+    });
+  });
+
+  describe('Post /rides', () => {
+    const mockBody = {
+      start_lat: 10,
+      start_long: 10,
+      end_lat: 10,
+      end_long: 10,
+      rider_name: 'mockrider',
+      driver_name: 'mockdriver',
+      driver_vehicle: 'mockdriver',
+    };
+    it('should create a ride', (done) => {
+      request(app)
+        .post('/rides')
+        .send(mockBody)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect((res) => {
+          delete res.body[0].created;
+        })
+        .expect(200, [{
+          rideID: 1001,
+          startLat: 10,
+          startLong: 10,
+          endLat: 10,
+          endLong: 10,
+          riderName: 'mockrider',
+          driverName: 'mockdriver',
+          driverVehicle: 'mockdriver',
+        }], done);
+    });
+    it('should return validation error for invalid latitude', (done) => {
+      request(app)
+        .post('/rides')
+        .send({ ...mockBody, start_lat: -200 })
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, {
+          error_code: 'VALIDATION_ERROR',
+          message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
+        }, done);
+    });
+    it('should return validation error for invalid longitude', (done) => {
+      request(app)
+        .post('/rides')
+        .send({ ...mockBody, end_long: -200 })
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, {
+          error_code: 'VALIDATION_ERROR',
+          message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
+        }, done);
+    });
+    it('should return validation error for invalid rider name', (done) => {
+      request(app)
+        .post('/rides')
+        .send({ ...mockBody, rider_name: null })
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, {
+          error_code: 'VALIDATION_ERROR',
+          message: 'Rider name must be a non empty string',
+        }, done);
+    });
+    it('should return validation error for invalid driver name', (done) => {
+      request(app)
+        .post('/rides')
+        .send({ ...mockBody, driver_name: null })
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, {
+          error_code: 'VALIDATION_ERROR',
+          message: 'Driver name must be a non empty string',
+        }, done);
+    });
+    it('should return validation error for invalid driver vehicle', (done) => {
+      request(app)
+        .post('/rides')
+        .send({ ...mockBody, driver_vehicle: null })
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, {
+          error_code: 'VALIDATION_ERROR',
+          message: 'Driver vehicle must be a non empty string',
+        }, done);
+    });
+  });
+
+  describe('Get /rides:id', () => {
+    it('should get rides for given id', (done) => {
+      request(app)
+        .get('/rides/100')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect((res) => {
+          delete res.body[0].created;
+        })
+        .expect(200, [{
+          rideID: 100,
+          startLat: 10,
+          startLong: 10,
+          endLat: 10,
+          endLong: 10,
+          riderName: 'test-rider',
+          driverName: 'test-driver',
+          driverVehicle: 'test-vehicle',
+        }], done);
+    });
+
+    it('should return empty array for invalid id', (done) => {
+      request(app)
+        .get('/rides/10000')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, {
+          error_code: 'RIDES_NOT_FOUND_ERROR',
+          message: 'Could not find any rides',
+        }, done);
     });
   });
 });
