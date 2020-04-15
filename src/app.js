fe845ee4e022f47ef3aa2072a7bbeb9ecd1e45b5
Aditi Toshniwal <adititoshniwal5@gmail.com>
2020-04-15 22:27:39 +0800

    refactor code to remove top level dependency

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ const bodyParser = require('body-parser');
 
 const jsonParser = bodyParser.json();
 
-const pageSize = 100;
+const getRides = require('./rides/get_rides');
+const createRide = require('./rides/create_ride');
+const getRideByID = require('./rides/get_ride_by_id');
 
 const logger = winston.createLogger({
   transports: [
@@ -17,126 +19,14 @@ const logger = winston.createLogger({
 });
 
 module.exports = (db) => {
-  app.get('/health', (req, res) => {
+  app.get('/health', (_, res) => {
     logger.info('Health Check!!');
     res.send('Healthy');
   });
 
-  app.post('/rides', jsonParser, (req, res) => {
-    const startLatitude = Number(req.body.start_lat);
-    const startLongitude = Number(req.body.start_long);
-    const endLatitude = Number(req.body.end_lat);
-    const endLongitude = Number(req.body.end_long);
-    const {
-      body: {
-        start_lat: startLat,
-        start_long: startLong,
-        end_lat: endLat,
-        end_long: endLong,
-        rider_name: riderName,
-        driver_name: driverName,
-        driver_vehicle: driverVehicle,
-      },
-    } = req;
-
-    if (Math.abs(startLatitude) > 90 || Math.abs(startLongitude) > 180) {
-      return res.send({
-        error_code: 'VALIDATION_ERROR',
-        message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
-      });
-    }
-
-    if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
-      return res.send({
-        error_code: 'VALIDATION_ERROR',
-        message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
-      });
-    }
-
-    if (typeof riderName !== 'string' || riderName.length < 1) {
-      return res.send({
-        error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
-      });
-    }
-
-    if (typeof driverName !== 'string' || driverName.length < 1) {
-      return res.send({
-        error_code: 'VALIDATION_ERROR',
-        message: 'Driver name must be a non empty string',
-      });
-    }
-
-    if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
-      return res.send({
-        error_code: 'VALIDATION_ERROR',
-        message: 'Driver vehicle must be a non empty string',
-      });
-    }
-
-
-    const values = [startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle];
-
-    return db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function cb(err) {
-      if (err) {
-        return res.send({
-          error_code: 'SERVER_ERROR',
-          message: 'Unknown error',
-        });
-      }
-
-      return db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, (findErr, rows) => {
-        if (findErr) {
-          return res.send({
-            error_code: 'SERVER_ERROR',
-            message: 'Unknown error',
-          });
-        }
-        return res.send(rows);
-      });
-    });
-  });
-
-  app.get('/rides', (req, res) => {
-    const prevID = req.query.prev || 0;
-    const values = [prevID, pageSize];
-    db.all('SELECT * FROM Rides where rideID > ? limit ?', values, (err, rows) => {
-      if (err) {
-        return res.send({
-          error_code: 'SERVER_ERROR',
-          message: 'Unknown error',
-        });
-      }
-      if (rows.length === 0) {
-        return res.send({
-          error_code: 'RIDES_NOT_FOUND_ERROR',
-          message: 'Could not find any rides',
-        });
-      }
-
-      return res.send(rows);
-    });
-  });
-
-  app.get('/rides/:id', (req, res) => {
-    db.all('SELECT * FROM Rides WHERE rideID= ?', req.params.id, (err, rows) => {
-      if (err) {
-        return res.send({
-          error_code: 'SERVER_ERROR',
-          message: 'Unknown error',
-        });
-      }
-
-      if (rows.length === 0) {
-        return res.send({
-          error_code: 'RIDES_NOT_FOUND_ERROR',
-          message: 'Could not find any rides',
-        });
-      }
-
-      return res.send(rows);
-    });
-  });
+  app.post('/rides', jsonParser, createRide(db));
+  app.get('/rides', getRides(db));
+  app.get('/rides/:id', getRideByID(db));
 
   return app;
 };
---
+++ src/rides/get_rides.js
@@ -0,0 +1,23 @@
+const defaultPageSize = 100;
+
+module.exports = (db) => (req, res) => {
+  const prevID = req.query.prev || 0;
+  const pageSize = req.query.pageSize || defaultPageSize;
+  const values = [prevID, pageSize];
+  db.all('SELECT * FROM Rides where rideID > ? limit ?', values, (err, rows) => {
+    if (err) {
+      return res.send({
+        error_code: 'SERVER_ERROR',
+        message: 'Unknown error',
+      });
+    }
+    if (rows.length === 0) {
+      return res.send({
+        error_code: 'RIDES_NOT_FOUND_ERROR',
+        message: 'Could not find any rides',
+      });
+    }
+
+    return res.send(rows);
+  });
+};
